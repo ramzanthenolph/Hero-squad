b623d75e17d05a1734c2681779e15cdc58ddbde6
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-08 07:17:50 +0300

    add private variables to hero class

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,13 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private static List<Hero> heroList = new ArrayList<>();
+    private int heroId;
 }
