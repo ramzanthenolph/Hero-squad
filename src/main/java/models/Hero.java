21bed4d92a43e06f5790843eab325a5fbfb19ae7
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-08 07:29:23 +0300

    add constructor to class Hero

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -10,4 +10,24 @@ public class Hero {
     private String weakness;
     private static List<Hero> heroList = new ArrayList<>();
     private int heroId;
+    private String squadAssemble;
+    private static boolean duplicate = false;
+
+    public Hero(String name, int age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+        findDuplicateHero(this);
+
+        if (duplicate) {
+            System.out.println("Duplicate Hero!!");
+        } else {
+            heroList.add(this);
+            this.heroId = heroList.size();
+            this.squadAssemble = "";
+        }
+    }
+
+    
 }
