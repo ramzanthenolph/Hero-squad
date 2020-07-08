e988d1788cf9e03d55ca2edd4ac8e515dcbb33c8
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-08 08:24:50 +0300

    add squad costructor

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -12,5 +12,18 @@ public class Squad {
     private int squadId;
     private boolean isSquadFull = false;
 
-    
+    public Squad(String squadName,String cause, Hero hero) {
+        this.squadName = squadName;
+        this.cause = cause;
+        crossCheckHero(hero.getHeroId());
+
+        if (isRegisteredHero) {
+            hero.setSquadAlliance(squadName);
+            heroMembers.add(hero);
+            squadList.add(this);
+            this.squadId = squadList.size();
+        } else {
+            System.out.println("Your Hero isn't registered");
+        }
+    }
 }
