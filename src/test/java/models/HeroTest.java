feff77608a84d98fbd5b5d95975e0bb0a131c6c7
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-08 07:44:02 +0300

    add test to get namme

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -6,9 +6,26 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HeroTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    private Hero setupNewHero() {
+        return new Hero("Ironman", 45, "Money", "rust");
+    }
+
+    private Hero setupHero2() {
+        return new Hero("Superman", 180, "Almost everything", "Kryptonite");
+    }
+
     @Test
     public void heroInstantiatesCorrectly() {
         Hero newHero = setupNewHero();
         assertTrue(newHero instanceof Hero);
+    }
+    @Test
+    public void getHeroName_true() {
+        Hero newHero = setupNewHero();
+        assertTrue(newHero.getName() instanceof String);
     }
 }
