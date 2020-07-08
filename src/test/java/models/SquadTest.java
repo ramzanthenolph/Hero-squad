60a8520e73d95eb05cb72cf6318e9a176af56abc
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-08 08:31:45 +0300

    add test for instantiation

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,7 +1,34 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SquadTest {
+    @Before
+    public void setUp() throws Exception {
+    }
 
+    private Hero setupNewHero() {
+        return new Hero("Batman", 45, "Money", "Loneliness");
+    }
+
+    private Hero setupHero2() {
+        return new Hero("Iron Man", 30, "Money", "Ego");
+    }
+
+    private Squad setupNewSquad(Hero hero) {
+        return new Squad("Justice League", "Just Exist", hero);
+    }
+
+    private Squad setupNewSquad2(Hero hero) {
+        return new Squad("Avengers", "Just Exist again", hero);
+    }
+    @Test
+    public void squadInstanciatedCorrectly() {
+        Squad squad = setupNewSquad(setupNewHero());
+        assertTrue(squad instanceof Squad);
+    }
 }
