bb95f36b701a8e641bccd4b3dac52e47807bc424
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-08 07:08:24 +0300

    add test  to see if it istantiates correctly

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,14 @@
 package models;
 
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HeroTest {
-
+    @Test
+    public void heroInstantiatesCorrectly() {
+        Hero newHero = setupNewHero();
+        assertTrue(newHero instanceof Hero);
+    }
 }
