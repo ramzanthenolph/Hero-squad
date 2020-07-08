1524720f42fe22ed371caddb60fb361fdafafc32
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-08 14:33:53 +0300

    add get request to forms and index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,6 +12,30 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args){
+        staticFileLocation("/public");
 
+        ProcessBuilder process = new ProcessBuilder();
+        int port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/hero-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/squad-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
