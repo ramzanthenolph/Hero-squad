01781366c59856c8e9e8a9530348a48f6002b6ec
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-08 16:30:56 +0300

    add function to get value from forms

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -37,5 +37,17 @@ public class App {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "squad-form.hbs");
         }, new HandlebarsTemplateEngine());
+        get("/hero", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            int age = Integer.parseInt(request.queryParams("age"));
+            String power = request.queryParams("power");
+            String weakness = request.queryParams("weakness");
+            model.put("name", name);
+            model.put("age", age);
+            model.put("power", power);
+            model.put("weakness", weakness);
+            return new ModelAndView(model, "hero.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
